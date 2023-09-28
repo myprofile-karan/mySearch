3961e053f02c499637e443708b45990cf92575cd
myprofile-karan <vkaran242@gmail.com>
2023-09-28 19:14:37 +0530

    sat up things

--- src/components/Datafetch.jsx
+++ src/components/Datafetch.jsx
@@ -17,11 +17,11 @@ const Datafetch = () => {
   }, []);
   return (
     
-    <div>
+    <div className="container mx-auto text-center">
       <input
         type="text"
         placeholder="search..."
-        style={{ border: "1px solid gray",margin:'50px 570px'}}
+        style={{ border: "1px solid gray",margin:'50px 0'}}
         value={text}
         onChange={(e) => setText(e.target.value)}
       />
@@ -32,8 +32,8 @@ const Datafetch = () => {
         .map((item) => {
           return (
             <>
-                <div className="col-3">
-                <div className="card px-3">
+                <div className="col-6 col-sm-3 col-md-3 col-lg-3">
+                <div className="card px-3 bg-light">
                     <img src={item.image} width="100%" height="200px" alt="" />
                     <h6>Name: {item.firstName} {item.lastName}</h6>
                     <span>Age: {item.age}</span>
